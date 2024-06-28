a6d67f496bf35c87a567191b5d36a1ad0d792b6f
Jatin200521 <166749813+Jatin200521@users.noreply.github.com>
2024-06-28 23:16:14 +0530

    Update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Spline from "@splinetool/react-spline";
+import "./Style.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <Spline scene="https://prod.spline.design/Hzf0alncEtpS4fkw/scene.splinecode" />
+      </div>
+    </>
+  );
+}
