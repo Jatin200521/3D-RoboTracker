6a6b733385c560baab0ecae1df644242d6eea52f
Jatin200521 <166749813+Jatin200521@users.noreply.github.com>
2024-07-04 22:36:28 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Spline from "@splinetool/react-spline";
 import "./Style.css";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 
 export default function App() {
   return (
     <>
+      <Header />
+      <Main />
+      <Footer />
       <div className="App">
         <Spline scene="https://prod.spline.design/Hzf0alncEtpS4fkw/scene.splinecode" />
       </div>
---
+++ src/Components/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../Css/Main.css";
+
+function Main() {
+  return <div>Main</div>;
+}
+
+export default Main;
