597f707734306e240cbf02479b3ded9078432759
Jatin200521 <166749813+Jatin200521@users.noreply.github.com>
2024-07-04 22:37:56 +0530

    Update

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../Css/Main.css";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="App">
+      <Spline scene="https://prod.spline.design/Hzf0alncEtpS4fkw/scene.splinecode" />
+    </div>
+  );
 }
 
 export default Main;
